0c2bee125ec99ee05adf3d39bbdd756f9e794c37
Zerensky <pure.evil97@mail.ru>
2022-08-23 18:31:05 +0600

    первое дз

---
+++ HW1/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Задача 66: Задайте значения M и N. Напишите программу, 
+которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+int f(int m, int n) 
+{ 
+    if (n == m) 
+        return n; 
+
+    return f(m + 1, n) + m;  
+} 
+
+int m = Convert.ToInt32(Console.ReadLine()); 
+int n = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine(f(m, n));
